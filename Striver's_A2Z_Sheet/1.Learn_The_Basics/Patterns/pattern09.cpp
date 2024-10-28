6e4179eb08fc05c444cc3a5dc70f70ccba90402c
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-10-29 03:39:57 +0530

    Create pattern09.cpp

---
+++ Striver's_A2Z_Sheet/1.Learn_The_Basics/Patterns/pattern09.cpp
@@ -0,0 +1,43 @@
+/* 
+Pattern-9
+    *
+   ***
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    *
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void printPattern() {
+ int n=5;
+ for(int i=1; i<=n; i++) {
+  for(int j=n; j>i; j--) {
+   cout << " ";
+  }
+  for(int k=1; k<=(2*i)-1; k++) {
+   cout << "*";
+  }
+  cout << endl;
+ } 
+ for(int i=n; i>=1; i--) {
+  for(int j=n; j>i; j--) {
+   cout << " ";
+  }
+  for(int k=1; k<=(2*i)-1; k++) {
+   cout << "*";
+  }
+  cout << endl;
+ }
+}
+
+int main() {
+ printPattern();
+ return 0;
+}
