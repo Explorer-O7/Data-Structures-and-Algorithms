a5dc015df4e9015716c568d6527df3a12438ba14
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-10-29 03:09:01 +0530

    Create pattern02.cpp

---
+++ Striver's_A2Z_Sheet/1.Learn_The_Basics/Patterns/pattern02.cpp
@@ -0,0 +1,25 @@
+/* 
+Pattern-2
+*
+**
+***
+****
+*****
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+void printPattern() {
+ int n=5;
+ for(int i=0; i<n; i++) {
+  for(int j=0; j<=i; j++) {
+   cout << "*";
+  }
+  cout << endl;
+ }
+}
+
+int main() {
+ printPattern();
+ return 0;
+}
