ea7c6cae9456ebbd19950459241683882fbeb656
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-11-02 00:35:33 +0530

    Create count_digits.cpp

---
+++ Striver's_A2Z_Sheet/1.Learn_The_Basics/iv.Know_Basic_Maths/count_digits.cpp
@@ -0,0 +1,16 @@
+/*
+The below code example is about counting the number of digits in a number. The countDigit function takes an integer as an argument, and returns the sum of 1 and log10 of given number, which eventually is the number of digits in the given number.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int countDigits(int n) {
+ return log10(n)+1;
+}
+
+int main() {
+ int n = 12345678;
+ cout << countDigits(n);
+ return 0;
+}
