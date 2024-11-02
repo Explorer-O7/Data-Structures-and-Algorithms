c8148629aeb06e40edfa2e24c5b455f87781c875
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-11-03 02:27:04 +0530

    Create print_factors.cpp

---
+++ Striver's_A2Z_Sheet/1.Learn_The_Basics/iv.Know_Basic_Maths/print_factors.cpp
@@ -0,0 +1,26 @@
+/*
+The below code example prints all the factors of the given number. The printFactors function takes an integer as an argument and print it's factors by following an optimal approach.
+Example:
+ Input: n = 36
+ Output: 1 2 3 4 6 9 12 18 36
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void printFactors(int n) {
+ set<int> ls;
+ for(int i=1; i<=sqrt(n); i++) {
+  if(n%i == 0) {
+   ls.insert(i);
+   ls.insert(n/i);
+  }
+ }
+ for(auto it:ls) cout << it << " ";
+}
+
+int main() {
+ int n = 36;
+ printFactors(n);
+ return 0;
+}
