f4dad913c9c7c5e36c60a3b49fce648a861ed627
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-11-16 13:01:08 +0530

    Create find_highest_and_lowest_frequency.cpp

---
+++ Striver's_A2Z_Sheet/1.Learn_The_Basics/vi.Learn_Basic_Hashing/find_highest_and_lowest_frequency.cpp
@@ -0,0 +1,36 @@
+/*
+The below code example prints the element(s) having highest & lowest frequency in the given array. The frequency function takes two arguments, one is an integer array and second one is an integer.
+Example:
+ Input: arr[2,1,2,3,2,1] = {}, n = 6,
+ Output: 2 3
+ Explanation: The frequency of 2 is three (highest), and the frequency of 3 is one (lowest).
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void frequency(int arr[], int n) {
+ int minEle, maxEle, lowFreq=n, highFreq=0;
+ unordered_map<int, int> map;
+ for(int i=0; i<n; i++) {
+  map[arr[i]]++;
+ }
+ for(auto itr : map) {
+  if(itr.second < lowFreq) {
+   minEle = itr.first;
+   lowFreq = itr.second;
+  }
+  if(itr.second > highFreq) {
+   maxEle = itr.first;
+   highFreq = itr.second;
+  }
+ }
+ cout << maxEle << " " << minEle;
+}
+
+int main() {
+ int n = 6;
+ int arr[] = {10,5,10,15,10,5};
+ frequency(arr, n);
+ return 0;
+}
