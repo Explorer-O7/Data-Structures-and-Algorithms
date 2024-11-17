75a199779f69850a6574ba835c361e9f12ec6466
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-11-18 00:18:55 +0530

    Create insertion_sort.cpp

---
+++ Striver's_A2Z_Sheet/2.Learn_Important_Sorting_Techniques/i.Sorting_I/insertion_sort.cpp
@@ -0,0 +1,30 @@
+/*
+The below code example sorts the elements of the given array using insertion sort in ascending order. The insertionSort function takes two arguments, one is an integer array and second one is an integer.
+Example:
+ Input: arr[] = {-2,4,-6,2,-4,6}, n = 6
+ Output: -6 -4 -2 2 4 6
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+ for(int i=0; i<n; i++) {
+  int j = i;
+  while(j>0 && arr[j-1]>arr[j]) {
+   arr[j-1] = arr[j-1]+arr[j];
+   arr[j] = arr[j-1]-arr[j];
+   arr[j-1] = arr[j-1]-arr[j];
+   j--;
+  }
+ }
+}
+
+int main() {
+ int n = 6;
+ int arr[] = {13,46,24,52,20,9};
+ insertionSort(arr, n);
+ for(int ele : arr)
+  cout << ele << " ";
+ return 0;
+}
