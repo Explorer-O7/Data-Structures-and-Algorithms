201e241ce09d20d584b7ea7537cada0007a33d8e
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-12-26 00:29:32 +0530

    Create recursive_bubble_sort.cpp

---
+++ Striver's_A2Z_Sheet/2.Learn_Important_Sorting_Techniques/ii.Sorting_II/recursive_bubble_sort.cpp
@@ -0,0 +1,29 @@
+/*
+The below code example implements the recursive bubble sort. The bubbleSort function takes two arguments, an integer array, and an integer, it sorts the given array in ascending order using recursion.
+  Input: arr[] = {13,46,24,52,20,9}, n = 6
+  Output: 9 13 20 24 46 52
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+ if(n == 1) return;
+ for(int i=0; i<n-1; i++) {
+  if(arr[i] > arr[i+1]) {
+   int temp = arr[i];
+   arr[i] = arr[i+1];
+   arr[i+1] = temp;
+  }
+ }
+ bubbleSort(arr, n-1);
+}
+
+int main() {
+ int n = 6;
+ int arr[] = {13,46,24,52,20,9};
+ bubbleSort(arr, n);
+ for(int itr : arr)
+  cout << itr <<" ";
+ return 0;
+}
