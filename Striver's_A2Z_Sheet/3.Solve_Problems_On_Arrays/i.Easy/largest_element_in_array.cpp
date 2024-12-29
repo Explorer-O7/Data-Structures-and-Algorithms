35e040a0f2c7a448c2679ebce2c70b128a456943
Shubham Kushwah <112422601+Explorer-O7@users.noreply.github.com>
2024-12-29 21:27:55 +0530

    Update largest_element_in_array.cpp

--- Striver's_A2Z_Sheet/3.Solve_Problems_On_Arrays/i.Easy/largest_element_in_array.cpp
+++ Striver's_A2Z_Sheet/3.Solve_Problems_On_Arrays/i.Easy/largest_element_in_array.cpp
@@ -1,9 +1,9 @@
 /*
 The below code example finds the largest element in the given array. The findLargest function takes two arguments, one is an integer array, and another is an integer, it finds and returns the largest element present in the given array.
 Example:
-  Input: arr[] = {15, 69, 0, -4, 9, -13}, n = 6
-  Output: 69
-  Explanation: 69 is the largest element amongst all the array elements.
+ Input: arr[] = {15, 69, 0, -4, 9, -13}, n = 6
+ Output: 69
+ Explanation: 69 is the largest element amongst all the array elements.
 */
 
 #include<bits/stdc++.h>
